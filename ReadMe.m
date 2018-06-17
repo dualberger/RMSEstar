2e4cf5454c60f2d17ed9a5b3f58cb189c150ff49
dualberger <dualberger@gmail.com>
2018-06-17 21:36:49 +0200

    add ReadMe and update some code comments

---
+++ ReadMe.m
@@ -0,0 +1,7 @@
+% This folder contains performance measures as Accuracy, Monotonicity, 
+% Consistency and RMSE* that were used in Biberger et al. (2018)
+% All files, with exception of rankcor.m, which was implemented by Rainer Huber,
+% were implemented by Thomas Biberger
+% 
+% eval_AQ_databases.m gives an example how to run this code
+% 
